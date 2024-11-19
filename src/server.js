9161a35b2890695fda253c92729c59ac61cb16ee
DESKTOP-VN3VINC\Дмитро <kukuliak.dmytro.clg@chnu.edu.ua>
2024-11-19 11:18:52 +0200

    Feat: added GET methods for the server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import * as http from 'http';
-import * as fs from 'fs';
 const data=[
     {
       "id": "10",
@@ -59,18 +58,44 @@ const data=[
     }
   ]
 
+const getAllBooks= ()=>{
+  return JSON.stringify(data)
+}
+const getBookById = (receivedId) => {
+  // find accepts a testing function
+  // it check each and every element, each book
+  // and checks if each book's if matches
+  const book = data.find(book => String(book.id) === String(receivedId));
+  if (book) {
+      console.log(book);
+      return JSON.stringify(book);
+  }
+  return JSON.stringify({ message: "Book not found" });
+};
 const server = http.createServer((req,res)=>{
-    if(req.method==="GET" && req.url==="/books"){
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.write(JSON.stringify(data));
-        res.end();
-    }else{
-        res.writeHead(404,{'Content-Type':'application/json'});
-        res.write(JSON.stringify({message:"Route not found"}));
-        res.end();
-    }
+   if(req.method==='GET' && req.url==='/books' )
+  {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(getAllBooks());
+    res.end();
+  }else if( req.method==='GET' && req.url.match(/\/books\/([0-9]+)/)){
+    const splits=req.url.split('/')
+    const id=splits[splits.length - 1]
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    res.write((getBookById(id)))
+    res.end()
+  }
+  else if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: "This is the API root" }));
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: "Route not found" }));
+    res.end();
+
+  }
 })
 server.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
-console.log(data[1]);
